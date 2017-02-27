7556dad6d859fcb8b9ed8bdc45db56d3b2f67212
zhanshu1 <1054258896@qq.com>
2017-02-27 15:13:00 +0800

    测试

---
+++ ceshi/ceshi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ceshi
+//
+//  Created by zhanshu1 on 2017/2/27.
+//  Copyright © 2017年 zhanshu1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
